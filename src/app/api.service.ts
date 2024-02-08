34bc12b29a6b72674ce873abad6389c457a67b2a
Klaudia <klaudia.wroblewska@luminator.com>
2024-02-08 12:14:30 +0100

    added fake api!

---
+++ src/app/api.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiKey = 'd8d8bd8fc9a245def8c2bd16cb32ba83';
+  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+  constructor(private http: HttpClient) { }
+
+  getWeather(city: string): Observable<any> {
+    const url = `${this.apiUrl}?q=${city}&appid=${this.apiKey}`;
+    return this.http.get(url);
+  }
+}
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.scss']
 })
-export class WeatherComponent {
+export class WeatherComponent implements OnInit {
+  city = 'Stockholm';
+  weatherData: any;
 
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.getWeather();
+  }
+
+  getWeather(): void {
+    this.apiService.getWeather(this.city)
+      .subscribe(data => {
+        this.weatherData = data;
+        console.log(this.weatherData);
+      });
+  }
 }
