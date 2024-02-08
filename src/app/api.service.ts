3772b3352da160de05e2c5b311f72ebef8e165da
Klaudia <klaudia.wroblewska@luminator.com>
2024-02-08 16:01:40 +0100

    try added mock

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,3 +1,26 @@
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ApiService {
+
+//   private apiUrl = 'https://9f55a98d-9acb-483c-a248-f2abef55797f.mock.pstmn.io';
+
+//   constructor(private http: HttpClient) { }
+
+//   getWeather(city: string): Observable<any> {
+//     // const url = `${this.apiUrl}/weather?q=${city}`;
+//     const url = `${this.apiUrl}/weather?q=`;
+//     // const url = `${this.apiUrl}?q=${city}&appid=${this.apiKey}`;
+//     return this.http.get(url);
+//   }
+// }
+
+
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -6,13 +29,16 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private apiKey = 'd8d8bd8fc9a245def8c2bd16cb32ba83';
-  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+  private apiUrl = 'https://your-base-url.com'; // Zastąp 'your-base-url.com' odpowiednim adresem bazowym
 
   constructor(private http: HttpClient) { }
 
-  getWeather(city: string): Observable<any> {
-    const url = `${this.apiUrl}?q=${city}&appid=${this.apiKey}`;
+  getWeather(randomLatitude: number, randomLongitude: number): Observable<any> {
+    const url = `${this.apiUrl}/weather?lat=${randomLatitude}&lng=${randomLongitude}`;
     return this.http.get(url);
   }
 }
+
+
+
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,3 +1,36 @@
+// import { Component, OnInit } from '@angular/core';
+// import { ApiService } from '../api.service';
+
+// @Component({
+//   selector: 'app-weather',
+//   templateUrl: './weather.component.html',
+//   styleUrls: ['./weather.component.scss']
+// })
+// export class WeatherComponent implements OnInit {
+//   city = 'Stockholm';
+//   weatherData: any;
+
+//   constructor(private apiService: ApiService) { }
+
+//   ngOnInit(): void {
+//     this.getWeather();
+//   }
+
+//   getWeather(): void {
+//     this.apiService.getWeather(this.city)
+//       .subscribe(
+//         data => {
+//           console.log('Received weather data:', data); // Wyświetl otrzymane dane przed ich przetworzeniem
+//           this.weatherData = data;
+//         },
+//         error => {
+//           console.error('Error fetching weather data:', error); // Wyświetl błąd, jeśli wystąpił
+//         }
+//       );
+//   }
+// }
+
+
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 
@@ -7,7 +40,6 @@ import { ApiService } from '../api.service';
   styleUrls: ['./weather.component.scss']
 })
 export class WeatherComponent implements OnInit {
-  city = 'Stockholm';
   weatherData: any;
 
   constructor(private apiService: ApiService) { }
@@ -17,10 +49,23 @@ export class WeatherComponent implements OnInit {
   }
 
   getWeather(): void {
-    this.apiService.getWeather(this.city)
+    const randomLatitude = this.generateRandomLatitude();
+    const randomLongitude = this.generateRandomLongitude();
+
+    this.apiService.getWeather(randomLatitude, randomLongitude)
       .subscribe(data => {
         this.weatherData = data;
         console.log(this.weatherData);
       });
+  }
+
+  private generateRandomLatitude(): number {
+    // Logika generowania losowej szerokości geograficznej
+    return Math.random() * (90 - (-90)) + (-90);
+  }
+
+  private generateRandomLongitude(): number {
+    // Logika generowania losowej długości geograficznej
+    return Math.random() * (180 - (-180)) + (-180);
   }
 }
