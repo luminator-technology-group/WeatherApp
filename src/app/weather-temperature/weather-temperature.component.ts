3e57adef9d523c93257d0f90c6e2ac2e4f00cf15
Klaudia <klaudia.wroblewska@luminator.com>
2024-02-14 16:17:44 +0100

    added unit

--- src/app/weather-temperature/weather-temperature.component.ts
+++ src/app/weather-temperature/weather-temperature.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-weather-temperature',
-  template: '<p> Temperature: {{weatherTemperature}}</p>',
+  template: '<p> {{weatherTemperature}} °C </p>',
   styleUrls: ['./weather-temperature.component.scss']
 })
 export class WeatherTemperatureComponent {
