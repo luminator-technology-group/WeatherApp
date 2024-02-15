d04320a2bdb198fa2b8d31bc2e53c562d6aa6931
KlaudiaLum <158457058+KlaudiaLum@users.noreply.github.com>
2024-02-15 14:29:16 +0100

    Display Wind with Mocked data (#3)
    * added feature for the component responsible for displaying wind
    
    * delete unnecessary file
    
    * deleted empty file
    
    * added icon for wind
    
    * resolved conflicts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+
 import { ApiService } from './api.service';
 
 @Component({
@@ -7,13 +9,16 @@ import { ApiService } from './api.service';
     <div>
   <app-weather-temperature [weatherTemperature] = "weatherTemperature"></app-weather-temperature>
   <app-weather-icon [weatherIconValue]="weatherIconValue"></app-weather-icon>
+  <app-weather-wind [weatherWind]= "weatherWind"></app-weather-wind>
 </div>
   `,
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'WeatherApp';
-  weatherData :any;
+
+  weatherData: any;
+  weatherWind: number = 0;
   weatherTemperature:number = 0;
   weatherIconValue: number = 0;
   randomLatitude: number = 0;
@@ -33,9 +38,12 @@ export class AppComponent {
         console.log(this.weatherData);
   
         this.weatherTemperature = this.weatherData.temp;
+        this.weatherWind = this.weatherData.windSpd;
         if (this.weatherData && this.weatherData.wsymb) {
           this.weatherIconValue = this.weatherData.wsymb;
         }
       });
   }
+  
+
 }
---
+++ src/app/weather-wind/weather-wind.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-wind',
+  template: `
+  <div class="wind-container">
+    <img class="wind-icon" src="/assets/wind.svg" alt="wind" />
+    <p> {{weatherWind}} m/s </p>
+  </div>`,
+  styleUrls: ['./weather-wind.component.scss']
+})
+export class WeatherWindComponent {
+@Input() weatherWind: number = 0
+
+}
