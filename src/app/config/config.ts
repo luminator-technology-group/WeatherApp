d0c64a8dac39b866ee3e36e629c0e82dc8153471
KlaudiaLum <klaudia.wroblewska@luminator.com>
2024-02-23 10:26:32 +0100

    try fix the problem

---
+++ src/app/config/config.ts
@@ -0,0 +1,7 @@
+const config = 
+  {
+    "hostname": "ltg-mss-dev-mosquitto-01.northeurope.cloudapp.azure.com",
+    "port": 1884,
+    "username": "ltg-mss-dev-mosquitto-01",
+    "password": "yjURZS2DbaQKbX5"
+  }
