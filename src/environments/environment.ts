d1dcfe9c410136575409ff1bc4d4a035a799aca7
KlaudiaLum <158457058+KlaudiaLum@users.noreply.github.com>
2024-02-27 14:27:22 +0100

    Connect libpis (#21)
    * libpis is connecting
    
    * declaration file for module paho-mqtt and added missin config in env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,12 @@
 export const environment = {
-    production: false,
-    apiBasePath: 'http://localhost:8080/dynamic-weather/api/weather/location?cityName=herrljunga',
-  }
+  production: false,
+  apiBasePath:
+    'http://localhost:8080/dynamic-weather/api/weather/location?cityName=herrljunga',
+
+  mqtt: {
+    hostname: 'ltg-mss-dev-mosquitto-01.northeurope.cloudapp.azure.com',
+    port: 1884,
+    username: 'ltg-mss-dev-mosquitto-01',
+    password: 'yjURZS2DbaQKbX5',
+  },
+};
