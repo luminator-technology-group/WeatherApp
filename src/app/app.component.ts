b6fa34a96d45f4c428e8d4cde67568247f1627b3
KlaudiaLum <klaudia.wroblewska@luminator.com>
2024-03-12 12:18:51 +0100

    fixed problems with bulit in pipline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './api.service';
 import { environment } from 'src/environments/environment';
+import { LuminatorWindow } from './app.model';
 import { CoordinatesService } from './coordinates.service';
 import { StopListService } from './stop-list.service';
 
 @Component({
   selector: 'app-root',
   template: `
-
-    <!-- <app-weather-temperature [weatherTemperature]="weatherTemperature"></app-weather-temperature>
+     <!-- <app-weather-temperature [weatherTemperature]="weatherTemperature"></app-weather-temperature>
     <app-weather-icon [weatherIconValue]="weatherIconValue"></app-weather-icon>
 <app-weather-wind [weatherWind]="weatherWind"></app-weather-wind>
 <app-lat-lng [coordinates]="coordinates"></app-lat-lng> -->
 <app-stop-list [stops]="stops"></app-stop-list>
-    <div class="top-container">
-      <app-final-destinationclass="final-destination"[finalDestinationName]="finalDestinationName"></app-final-destination>
-      <app-current-time class="current-time"></app-current-time>
-    </div>
+<div class="top-container">
+  <app-final-destination class="final-destination" [finalDestinationName]="finalDestinationName"></app-final-destination>
+  <app-current-time class="current-time"></app-current-time>
+</div>
+
+ 
+
+     
   `,
   styleUrls: ['./app.component.scss'],
 })
@@ -31,13 +35,15 @@ export class AppComponent implements OnInit {
   latitude = 0;
   longitude = 0;
   mqttConfig = environment.mqtt;
-  coordinates: { latitude: number; longitude: number }[] = [];
+  coordinates: { latitude: number; longitude: number; }[] =[];
   finalDestinationName = '';
+
 
   constructor(
     private apiService: ApiService,
     private coordinatesService: CoordinatesService,
     private stopListService: StopListService,
+
   ) {}
 
   ngOnInit(): void {
@@ -53,21 +59,20 @@ export class AppComponent implements OnInit {
       if (this.weatherData && this.weatherData.wsymb) {
         this.weatherIconValue = this.weatherData.wsymb;
       }
-      console.log('Weather', data);
+      console.log( "Weather",data)
     });
   }
 
   // connectet libpis with mqtt broker - Please check if this connection is okay I'm not sure about this code.
   initConnection() {
     window.luminator.pis.init(this.mqttConfig);
-
     
     window.luminator.pis.client
       .updates()
       .subscribe({
         next: (state: any) => {
-          if (state) {
-            console.log('LIBPIS DATA', state);
+          if (state && state.stopList) {
+            console.log('LIBPIS DATA', state.stopList);
             this.handleCoordinates(state); 
             this.handleStopListData(state);
           } else {
@@ -80,12 +85,10 @@ export class AppComponent implements OnInit {
       });
   }
 
-  // read Latitude and Longitude
+ // read Latitude and Longitude
   handleCoordinates(state: any): void {
     if (state.stopList && state.stopList.length > 0) {
-      const coordinate = this.coordinatesService.processCoordinates(
-        state.stopList,
-      );
+      const coordinate = this.coordinatesService.processCoordinates(state.stopList);
       if (coordinate) {
         this.coordinates = coordinate;
       } else {
@@ -94,10 +97,12 @@ export class AppComponent implements OnInit {
       }
     } else {
       console.log('StopList is either undefined or empty');
-      this.coordinates = [];
+      this.coordinates = []; 
     }
-    console.log('LIBPIS DATA', state);
-  }
+          console.log('LIBPIS DATA', state);
+         
+        }
+  
 
   // get stopList
 
@@ -108,12 +113,10 @@ export class AppComponent implements OnInit {
     // Update stop list
     this.stops = parsedStopList;
     this.stopListService.updateStops(parsedStopList);
-
-    // get final destination name
-    this.finalDestinationName = state.finalDestinationName;
   }
 
   parseStopList(stopList: any): any[] {
     return stopList; // By default, it returns the unprocessed stop list
+
   }
 }
